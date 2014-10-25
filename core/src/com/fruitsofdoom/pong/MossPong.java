41f5befd1c66e3f4aae3d20814a2a9818f043e4c
Moss Cantwell <mossman2142@gmail.com>
2014-10-25 17:23:55 +1300

    architecture change with working 2p mode

--- core/src/com/fruitsofdoom/pong/MossPong.java
+++ core/src/com/fruitsofdoom/pong/MossPong.java
@@ -1,6 +1,6 @@
 package com.fruitsofdoom.pong;
 
-import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Camera;
 import com.badlogic.gdx.graphics.GL20;
@@ -10,30 +10,16 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer.ShapeType;
 
-public class MossPong extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
-	ShapeRenderer shapeRenderer;
-	Camera camera;
-	@Override
-	public void create () {
-		batch = new SpriteBatch();
-		shapeRenderer = new ShapeRenderer();
-		camera = new OrthographicCamera(480,320);
-		
-	}
+public class MossPong extends Game{
 
 	@Override
-	public void render () {
-		
-		Gdx.gl.glClearColor(0, 0, 0, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		camera.update();
-		shapeRenderer.setProjectionMatrix(camera.combined);
-		 shapeRenderer.begin(ShapeType.Filled);
-		 shapeRenderer.setColor(1, 1, 1, 1);
-		 shapeRenderer.rect(200, -60, 20, 60);
-		 shapeRenderer.rect(-220, -60, 20, 60);
-		 shapeRenderer.end();
+	public void create() {
+		this.setScreen(new MainMenu(this));
 	}
+	public void render(){
+		super.render();
+	}
+	public void dispose() {
+        this.getScreen().dispose();
+    }
 }
